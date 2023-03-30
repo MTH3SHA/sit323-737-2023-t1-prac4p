4e9eef4b2c8dec2ee5c8b5b1a407f3bb25e139a3
MatheeshaPalliyaguruge <s222506155@deakin.edu.au>
2023-03-30 11:28:51 +1100

    first commit

--- app.js
+++
@@ -1,6 +0,0 @@
-var log=function(msg){
-    console.log("[log]: ", msg)
-}
-
-log("Hello World")
-log("Welcome to SIT323")
--- calculator.js
+++ calculator.js
@@ -1,21 +1,32 @@
 const express= require("express");
 const res = require("express/lib/response");
 const app = express();
-const add= (n1,n2) => {
-    return n1+n2;
+const addition= (num1,num2) => {
+    return num1+num2;
 }
-app.get("/add", (req,res) => {
+const subtraction= (num1,num2) => {
+    return num1-num2;
+}
+const multiplication= (num1,num2) => {
+    return num1*num2;
+}
+const division= (num1,num2) => {
+    return num1/num2;
+}
+
+//addition endpoint
+app.get("/addition", (req,res) => {
     try{
-    const n1= parseFloat(req.query.n1);
-    const n2= parseFloat(req.query.n2);
-    if(isNaN(n1)){
-        throw new Error("n1 incorrectly defined");
+    const num1= parseFloat(req.query.num1);
+    const num2= parseFloat(req.query.num2);
+    if(isNaN(num1)){
+        throw new Error("num1 is not properly defined. Retry again!");
     }
-    if(isNaN(n2)){
-        throw new Error("n2 incorrectly defined");
+    if(isNaN(num2)){
+        throw new Error("num2 is not properly defined. Retry again!");
     }
 
-    const result = add(n1,n2);
+    const result = addition(num1,num2);
     res.status(200).json({statuscocde:200, data: result});
     }catch(error){
         console.error(error)
@@ -23,7 +34,67 @@ app.get("/add", (req,res) => {
     }
 });
 
-const port=3040;
+//subtraction endpoint
+app.get("/subtraction", (req,res) => {
+    try{
+    const num1= parseFloat(req.query.num1);
+    const num2= parseFloat(req.query.num2);
+    if(isNaN(num1)){
+        throw new Error("num1 is not properly defined. Retry again!");
+    }
+    if(isNaN(num2)){
+        throw new Error("num2 is not properly defined. Retry again!");
+    }
+
+    const result = subtraction(num1,num2);
+    res.status(200).json({statuscocde:200, data: result});
+    }catch(error){
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString()})
+    }
+});
+
+//multiplication endpoint
+app.get("/multiplication", (req,res) => {
+    try{
+    const num1= parseFloat(req.query.num1);
+    const num2= parseFloat(req.query.num2);
+    if(isNaN(num1)){
+        throw new Error("num1 is not properly defined. Retry again!");
+    }
+    if(isNaN(num2)){
+        throw new Error("num2 is not properly defined. Retry again!");
+    }
+
+    const result = multiplication(num1,num2);
+    res.status(200).json({statuscocde:200, data: result});
+    }catch(error){
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString()})
+    }
+});
+
+//division endpoint
+app.get("/division", (req,res)=>{
+    try{
+        const num1= parseFloat(req.query.num1);
+        const num2= parseFloat(req.query.num2);
+        if(isNaN(num1)){
+            throw new Error("num1 is not properly defined. Retry again!");
+        }
+        if(isNaN(num2)){
+            throw new Error("num2 is not properly defined. Retry again!");
+        }
+    
+        const result = division(num1,num2);
+        res.status(200).json({statuscocde:200, data: result});
+        }catch(error){
+            console.error(error)
+            res.status(500).json({statuscocde:500, msg: error.toString()})
+        }
+});
+
+const port=4000;
 app.listen(port,()=>{
-    console.log("Hello I'm listening to port"+ port);
+    console.log("Hello I'm listening to port: "+ port);
 })
